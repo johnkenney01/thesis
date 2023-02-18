ed1553a77fa3da80917d3f9c522074d2c623f5ad
johnkenney01 <jkenn600@mail.endicott.edu>
2023-02-17 22:16:04 -0500

    Added Camera compatibility for levels class

--- classes/levels.lua
+++ classes/levels.lua
@@ -1,5 +1,7 @@
 require('miscellaneous/helpers')
 Object = require('classes/classic')
+camera = require("externalLibraries/camera")
+cam = camera()
 
 Level = Object:extend()
 
@@ -8,13 +10,27 @@ function Level.new(self, contents)
 end 
 
 function Level.draw(self)
+    
     for i = 1, #self.contents do
-        self.contents[i].draw(self.contents[i])
+        if self.contents[i].__type == "player" then 
+            cam:attach()
+                self.contents[i].draw(self.contents[i])
+            cam:detach()
+        else
+            self.contents[i].draw(self.contents[i])
+        end 
     end 
 end 
 
 function Level.update(self, dt)
     for i = 1, #self.contents do
         self.contents[i].update(self.contents[i], dt)
+        if self.contents[i].__type == "player" then 
+            cam:lookAt(self.contents[i].x, self.contents[i].y)
+        end 
     end 
+end 
+
+function Level.getContents(self,dt)
+    return self.contents
 end 
