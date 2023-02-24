7f65fd47c23b221e74f59db8f0a892eaf2e8c4a7
John Kenney <jkenn600@mail.endicott.edu>
2023-02-24 16:30:14 -0500

    New Class for managingSpecficInstancesOf tiledmaps

---
+++ classes/gameMap.lua
@@ -0,0 +1,16 @@
+-- Game Map | This will manage all functions for imported game maps
+Object = require('classes/classic')
+gameMap = Object:extend()
+
+function gameMap.new(self, pathToMap)
+    self.pathToMap = pathToMap
+    -- self.levelMap
+end 
+
+function gameMap.draw(self)
+
+end 
+
+function gameMap.update(self,dt)
+
+end 
