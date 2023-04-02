31dc8f087984b187040ff54e519d3abf666a68aa
johnkenney01 <jkenn600@mail.endicott.edu>
2023-04-01 22:50:15 -0400

    Added Physics to maps

--- classes/gameMap.lua
+++ classes/gameMap.lua
@@ -1,24 +1,37 @@
 -- Game Map | This will manage all functions for imported game maps
 Object = require('classes/classic')
 require('miscellaneous/helpers')
+require("classes/levels")
 gameMap = Object:extend()
 
-function gameMap.new(self, pathToMap)
+function gameMap.new(self, pathToMap, world)
     self.pathToMap = pathToMap
     self.map = sti(self.pathToMap)
-    self.scaleCoords={}
+    self.world = world
+    self.walls = {}
+    
+    if self.map.layers["Object"] then 
+        for i, obj in pairs(self.map.layers["Object"].objects) do
+            self.wall = self.world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+            self.wall:setType('static')
+            table.insert(self.walls, wall)
+        end 
+    end 
     
 end 
 
 function gameMap.draw(self)
     for layer = 1, #self.map.layers do
-        self.map:drawLayer(self.map.layers[layer])
+        if  self.map.layers[layer].name ~= "Object" then 
+            self.map:drawLayer(self.map.layers[layer])   
+        end 
     end 
-    
+    self.world:draw()
 end 
 
 function gameMap.update(self,dt)
-    self.scaleD(self)
+    
+    self.world:update(dt)
     self.map:update(dt)
 end 
 
@@ -26,8 +39,4 @@ end
 --HELPER FUNCTIONS FOR GAME MAP=
 --==============================
 
-function gameMap.scaleD(self)
-    -- self.scale.x = WINDOW_WIDTH / 1080
-    -- self.scale.y = WINDOW_HEIGHT / 720
-    love.graphics.scale(4,4)
-end 
+
